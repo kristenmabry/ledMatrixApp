b627729fc55b365c6f8ac54fe82badb5a7a8a5f2
kristenmabry <kristen@themabryfamily.com>
2020-06-30 19:40:24 -0600

    can pick colors and preview text

--- app/src/main/java/com/example/ledmatrix/ColorPickerActivity.java
+++ app/src/main/java/com/example/ledmatrix/ColorPickerActivity.java
@@ -2,13 +2,130 @@ package com.example.ledmatrix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 
-public class ColorPickerActivity extends AppCompatActivity {
+public class ColorPickerActivity extends AppCompatActivity implements View.OnClickListener {
+    final static public int KEY_PICKER_CODE = 1000;
+    final static public String KEY_PICKER_DATA = "color_values";
+    final static public String KEY_PICKER_NUM = "picker_num";
+    final static public String KEY_LINE_NUM = "line_num";
+    private int pickerNum;
+    private int lineNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
+        Intent intent = getIntent();
+        pickerNum = intent.getIntExtra(KEY_PICKER_NUM, 0);
+        lineNum = intent.getIntExtra(KEY_LINE_NUM, 1);
+        GridLayout grid = (GridLayout) findViewById(R.id.grid);
+        generateColors(grid);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra(KEY_LINE_NUM, lineNum);
+        returnIntent.putExtra(KEY_PICKER_NUM, pickerNum);
+        returnIntent.putExtra(KEY_PICKER_DATA, getColorValues(v.getId()));
+        setResult(Activity.RESULT_OK, returnIntent);
+        finish();
+    }
+
+    private View createSquare(int r, int g, int b) {
+        View square = new View(ColorPickerActivity.this);
+        int size = getResources().getDimensionPixelSize(R.dimen.color_picker_size);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(size, size);
+        int margins = getResources().getDimensionPixelSize(R.dimen.color_picker_margin);
+        params.setMargins(margins, margins, margins, margins);
+        square.setLayoutParams(params);
+        square.setBackgroundColor(Color.parseColor(intToHex(r, g, b)));
+        square.setId(getColorId(r, g, b));
+        square.setOnClickListener(this);
+        return square;
+    }
+
+    private int getColorId(int r, int g, int b) {
+        return (r * 100) + (g * 10) + b;
+    }
+
+    private int[] getColorValues(int id) {
+        int r = id / 100;
+        int g = (id % 100) / 10;
+        int b = id % 10;
+        int[] colors = { r, g, b };
+        return colors;
+    }
+
+    static public String intToHex(int r, int g, int b) {
+        String hexR = String.format("%02X", (int) (r * 255) / 7);
+        String hexG = String.format("%02X", (int) (g * 255) / 7);
+        String hexB = String.format("%02X", (int) (b * 255) / 7);
+        return "#" + hexR + hexG + hexB;
+    }
+
+    private void generateColors(GridLayout grid) {
+        int r;
+        int g;
+        int b;
+        for (r = g = b = 7; b >= 0; --b, --g, --r) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (r = 1, b = g = 0; r < 8; ++r) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (r = 7; g < 8; ++g) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (g = 7; r >= 0; --r) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (r = 0; b < 8; ++b) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (b = 7; g >= 0; --g) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (g = 0; r < 8; ++r) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (r = 7; b >= 0; --b) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (b = 0; b < 8; ++b, ++g) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (b = 7, g = r = 0; g < 8; ++r, ++g) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (g = 7, r = b = 6; b > 0; --r, --b) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (g = 7; g > 0; --g) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
+        for (b = 0; b < 8; ++b) {
+            View square = createSquare(r, g, b);
+            grid.addView(square);
+        }
     }
 }
--- app/src/main/java/com/example/ledmatrix/SendTextActivity.java
+++ app/src/main/java/com/example/ledmatrix/SendTextActivity.java
@@ -1,9 +1,13 @@
 package com.example.ledmatrix;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,15 +18,16 @@ public class SendTextActivity extends AppCompatActivity {
     private TextView line1Output;
     private TextView line2Output;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_text);
 
         line1 = (LineEditorComponent) findViewById(R.id.line1);
-        line1.init(getString(R.string.first_line));
+        line1.init(getString(R.string.first_line), 1);
         line2 = (LineEditorComponent) findViewById(R.id.line2);
-        line2.init(getString(R.string.second_line));
+        line2.init(getString(R.string.second_line), 2);
         line1Output = (TextView) findViewById(R.id.line1_output);
         line2Output = (TextView) findViewById(R.id.line2_output);
 
@@ -34,12 +39,57 @@ public class SendTextActivity extends AppCompatActivity {
             }
         });
 
+       previewText();
+
+        LineEditorComponent.OnChangeListener listener = new LineEditorComponent.OnChangeListener() {
+            public void onChange(int pickerNum, int lineNum) {
+                Intent intent = new Intent(SendTextActivity.this, ColorPickerActivity.class);
+                intent.putExtra(ColorPickerActivity.KEY_PICKER_NUM, pickerNum);
+                intent.putExtra(ColorPickerActivity.KEY_LINE_NUM, lineNum);
+                startActivityForResult(intent, ColorPickerActivity.KEY_PICKER_CODE);
+            }
+        };
+
+
+
+        line1.setOnChangeListener(listener);
+        line2.setOnChangeListener(listener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ColorPickerActivity.KEY_PICKER_CODE && resultCode == RESULT_OK) {
+            int[] values = data.getIntArrayExtra(ColorPickerActivity.KEY_PICKER_DATA);
+            int picker = data.getIntExtra(ColorPickerActivity.KEY_PICKER_NUM, 0);
+            int line = data.getIntExtra(ColorPickerActivity.KEY_LINE_NUM, 1);
+            if (line == 1) {
+                line1.setColor(picker, values);
+            } else {
+                line2.setColor(picker, values);
+            }
+        }
+    }
+
+    private void previewText() {
         final Button previewButton = (Button) findViewById(R.id.preview);
         previewButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                line1Output.setText(line1.getText());
-                line2Output.setText(line2.getText());
+                line1Output.setText(formatText(line1.getText(), line1.getHexColors()));
+                line1Output.setBackgroundColor(getResources().getColor(android.R.color.black));
+
+                line2Output.setText(formatText(line2.getText(), line2.getHexColors()));
+                line2Output.setBackgroundColor(getResources().getColor(android.R.color.black));
             }
         });
+    }
+
+    private Spanned formatText(String text, String[] colors) {
+        final String FONT_REPLACEMENT = "<font color='%1$s'>%2$s</font>";
+        String lineText = "";
+        for (int i = 0; i < 5; ++i) {
+            lineText += String.format(FONT_REPLACEMENT, colors[i], text.substring(i, i + 1));
+        }
+        return Html.fromHtml(lineText);
     }
 }
